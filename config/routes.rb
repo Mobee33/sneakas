15c308d32d7041fd0c92af4d9d91f046bbac0143
Mubeen Bankur <mobee33@hotmail.com>
2023-04-20 14:21:21 +0400

    donezo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sneakers#index'
+  resources :sneakers
 end
